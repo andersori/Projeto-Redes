f7c446963b06f64cb2fa03c0af423b452daf22f7
Anderson Soriano <andersori7@gmail.com>
2016-12-02 23:13:33 -0200

    Trabalho 2 Finalizado

--- Threads-C/main.c
+++ Threads-C/main.c
@@ -1,174 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <pthread.h>
+#include <semaphore.h>
+
+#include <windows.h>
 #include <time.h>
 
-//Definindo o tipo boleano
-typedef short bool;
-#define TRUE 1
-#define FALSE 0
+#define MAX_VERIFICADOS 10000
+int QTD_VERIFICADOS;
 
-//Maximo de numeros consumidos
-#define MAX_NUM_CONSUMIDO 10000
+int             *BUFFER;
+sem_t           SEMAFORO_s;
+sem_t           SEMAFORO_vazio;
+sem_t           SEMAFORO_cheio;
+int indice;
 
-//Parametros passado para as threads
-typedef struct{
-    short* sem;
-    int tam_memoria;
-}parametros;
+int c = 0;
 
-//Memoria compartilhada
-int* MEMORIA_COMP;
-
-//Numero de elementos consumidos
-int NUM_CONSUMIDO = 0;
-
-//Função que verifica se um numero é primo
-bool is_primo(int num){
+void is_primo(int num){
     int i;
     for(i=0 ; i < (num / 2 )+ 1; i++){
         if(num % 2 == 0)
-            return FALSE;
+            return;
     }
 
-    return TRUE;
+    printf("%d\n", num);
+    c++;
 }
 
-//Função que busca por uma posição livre no array compartilhado entre as threads (RETORNA O INDICE VAZIO)
-int pos_livre(int tam_memoria){
-    int i;
-    for(i = 0; i < tam_memoria; i++){
-        if(MEMORIA_COMP[i] == 0){
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-//Função que busca por uma posição ocupada no array compartilhado entre as threads (RETORNA UM INDICE OCUPADO PARA A CONSUMIDOR)
-int pos_ocupada(int tam_memoria){
-    int i;
-    for(i = 0; i < tam_memoria; i++){
-        if(MEMORIA_COMP[i] != 0){
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-//Thread que produzirá os numeros para colocar no array
-void* produzir(void* param_void){
-
-    //Convertendo o parametro passado para o tipo correto
-    parametros* p = (parametros*) param_void;
-
-    //Enquanto o numero maximo de itens consumidos não for alcansado
-    //Fique produzindo numeros aleatorios
-    while(NUM_CONSUMIDO <= MAX_NUM_CONSUMIDO){
-
-        //Buscando um indice da memoria conpartilhada que esteja livre
-        int pos_memoria_livre = pos_livre(p->tam_memoria);//RECEBE UM INDICE VAZIO
-
-        //Se uma posição foi encontrada e nenhuma outra thread estiver na região critica ele concegue entrar na regiao
-        if(pos_memoria_livre != -1 && p->sem[pos_memoria_livre] == TRUE){
-            //Região critica
-            p->sem[pos_memoria_livre] = FALSE;//pos_memoria_livre RECEBE O INDICE VAZIO
-            MEMORIA_COMP[pos_memoria_livre] = (rand() % 107) + 1;
-            p->sem[pos_memoria_livre] = TRUE;
-            //Fim da região critica
-        }
-
-    }
-
-    pthread_exit(NULL);
-}
-
-void* consumir(void* param_void){
-    parametros* p = (parametros*)param_void;
-
-    while(NUM_CONSUMIDO <= MAX_NUM_CONSUMIDO){
-        int pos_memoria_ocupada = pos_ocupada(p->tam_memoria);//RECEBE UM INDICE DO ARRAY PARA CONSUMIR
-
-        if(pos_memoria_ocupada != -1 && p->sem[pos_memoria_ocupada] == TRUE){//VERIFICA SE TEM INDICE OCUPADO E SE NAO TEM NINGUEM NA REGIÃO CRITICA
-            //Inicio região critica
-            p->sem[pos_memoria_ocupada] = FALSE;//SETA FALSE PARA NENHUMA OUTRA THREAD ENTRAR NESTA REGIÃO
-
-            int num_retirado = MEMORIA_COMP[pos_memoria_ocupada];//ARAMAZENA SOMENTE PARA VERIFICAR SE É PRIMO
-            MEMORIA_COMP[pos_memoria_ocupada] = 0;//SETA COMO CONSUMIDO
-
-            if(is_primo(num_retirado) == TRUE){
-                printf("%d\n", num_retirado);
-            }
-
-            NUM_CONSUMIDO++;//INCRMENTA PARA PARAR A EXECUÇAO QUANDO CHEGAR A 10000
-            p->sem[pos_memoria_ocupada] = TRUE;//SETA COMO TRUE PARA LIBERAR A REGIAO CRITICA PARA OUTRA THREAD
-            //Fim da região critica
-        }
-    }
-
-    pthread_exit(NULL);
-}
-
-int main(int argc, char* argv[])
+void *produzir()
 {
-    srand(time(NULL));//INICIALIZANDO O METODO DE GERAR NUMEROS ALEATORIOS
+    int n;
+    while(1){
+        n = (rand() % 107) + 1;
+        sem_wait(&SEMAFORO_vazio);
+        sem_wait(&SEMAFORO_s);
 
-    int tam_memoria;
+        //[-] INICIO da região critica
+
+        BUFFER[indice++] = n;
+
+        //[-] FIM da região critica
+
+        sem_post(&SEMAFORO_s);
+        sem_post(&SEMAFORO_cheio);
+
+        //printf("QTD_VERIFICADOS = %d\n", QTD_VERIFICADOS);
+        //printf("[-]Produio %d\n", n);
+        //Sleep(2000);
+    }
+
+    pthread_exit(NULL);
+}
+
+void *consumir()
+{
+    int n;
+    while(1){
+        sem_wait(&SEMAFORO_cheio);
+        sem_wait(&SEMAFORO_s);
+
+        //[-] INICIO da região critica
+
+        n = BUFFER[--indice];
+
+        QTD_VERIFICADOS++;
+        is_primo(n);
+        if(QTD_VERIFICADOS == MAX_VERIFICADOS){
+            printf("Quantidade de primos produzidos = %d.", c);
+            exit(0);
+        }
+
+        //[-] FIM da região critica
+
+        sem_post(&SEMAFORO_s);
+        sem_post(&SEMAFORO_vazio);
+
+        //printf("QTD_VERIFICADOS = %d\n", QTD_VERIFICADOS);
+        //printf("[-]C< %d\n", n);
+        //Sleep(2000);
+    }
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    indice = 0;
+    QTD_VERIFICADOS = 0;
+    srand(time(NULL));
+
+    pthread_t *produtor, *consumidor;
+
+    int qtd_memoria;
     int qtd_produtores;
     int qtd_consumidores;
 
-    printf("Tamanho do vetor: ");
-    scanf("%d", &tam_memoria);
-
-    printf("Quantidade de Threads Produtoras: ");
+    printf("Tamanho da memoria: ");
+    scanf("%d", &qtd_memoria);
+    printf("Quantidade de produtores: ");
     scanf("%d", &qtd_produtores);
-
-    printf("Quantidade de Threads Consumidoras: ");
+    printf("Quantidade de consumidores: ");
     scanf("%d", &qtd_consumidores);
 
-    //"calloc" aloca memoria para uma variavel e inicializa todas com zero
-    MEMORIA_COMP = (int*) calloc(tam_memoria, sizeof(int));//ALOCANDO O ARRAY COMPARTILHADO (REGIAO CRITICA)
+    BUFFER = (int*) calloc(qtd_memoria, sizeof(int));
+    produtor = (pthread_t*) malloc(qtd_produtores * sizeof(pthread_t));
+    consumidor = (pthread_t*) malloc(qtd_consumidores * sizeof(pthread_t));
 
-    //Alocando memoria para os vetores de threads
-    pthread_t* produtor     = (pthread_t*) malloc(qtd_produtores * sizeof(pthread_t));
-    pthread_t* consumidor   = (pthread_t*) malloc(qtd_consumidores * sizeof(pthread_t));
+    sem_init(&SEMAFORO_s, 0, 1);
+    sem_init(&SEMAFORO_vazio, 0, qtd_memoria);
+    sem_init(&SEMAFORO_cheio, 0, 0);
 
-    //Variavel para os "for"
     int i;
-
-    parametros p;
-
-    p.tam_memoria = tam_memoria;//RECEBENDO O TAMANHO DO ARRAY DIGITADO PELO USUARIO
-    p.sem = (short*) calloc(tam_memoria, sizeof(short));//ALOCANDO O ARRAY DE PERMISSAO DO ACESSO A REGIAO CRITICA (SHORT=boolean)
-
-    for(i = 0; i < p.tam_memoria; i++){
-        p.sem[i] = TRUE;//SETA TODO ARRAY COMO TRUE PARA DEIXA TODO MUNDO COM ACESSO PERMISIVEL
+    for(i = 0; i < qtd_produtores; i++){
+        pthread_create(&produtor[i], NULL, produzir, NULL);
+    }
+    for(i = 0; i <qtd_consumidores; i++){
+        pthread_create(&consumidor[i], NULL, consumir, NULL);
     }
 
-    //Criando as threads -> associando um função a uma thread
-    for(i=0; i<qtd_produtores;i++){//CRIANDO A QUANTIDADE DE THREAD QUE O USUARIO DIGITOU, A FUNCAO 'PRODUZIR'
-        pthread_create(&produtor[i], NULL, produzir, (void*)&p);
-    }
-    for(i=0; i<qtd_consumidores;i++){//THREADS CONSUMIDORES
-        pthread_create(&consumidor[i], NULL, consumir, (void*)&p);
-    }
-
-    //Executando as threads
-    for(i=0; i<qtd_produtores;i++){
+    for(i = 0; i < qtd_produtores; i++){
         pthread_join(produtor[i], NULL);
     }
-    for(i=0; i<qtd_consumidores;i++){
+    for(i = 0; i <qtd_consumidores; i++){
         pthread_join(consumidor[i], NULL);
     }
-
-    printf("\n\n\n\n\n\n\n\n\n\n");
-
-    for(i = 0; i < tam_memoria; i++){
-        printf("%d\n", MEMORIA_COMP[i]);
-    }
-
-    system("pause");
-    return 1;
+    return 0;
 }
